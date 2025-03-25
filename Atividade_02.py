befe6bbf2e9ee07108f28cad9c63294f8851b55e
Lucas Teixeira <lucasteixeira8784@gmail.com>
2025-03-25 18:35:09 -0300

    exercicios 5 e 6

--- Atividade_02.py
+++ Atividade_02.py
@@ -1,15 +1,42 @@
 #%%
 ## 1. Escreva uma função que receba uma lista de números e retorne outra lista com os números ímpares.
-import pandas as pd
+def definir_impares(lista):
+    resultado = []
+    for i in lista:
+        if i % 2 != 0:
+            resultado.append(i)
+    return resultado
 
-def filtrar_impares(lista):
-    sr = pd.Series(lista)
-    impar = sr[sr % 2 != 0].to_list()
-    return impar
+a = [3, 4, 5, 6, 7, 8, 9, 90, 10]
+b = [5, 67, 8, 89,9 ,12, 23, 4, 5,]
+definir_impares(a)
+#%%
+## 2. Escreva uma função que receba uma lista de números e retorne outra lista com os números primos presentes.
 
-lista = [1, 2, 3, 45, 569, 7, 8, 9 , 14, 34, 567]
-resultado = filtrar_impares(lista)
-print(f'Lista: {lista}\nNúmeros impares: {resultado}')
+def ePrimo(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def listaPrimos(lst):
+    elst = []
+    for i in lst:
+        if ePrimo(i):
+            elst.append(i)
+    return elst
+
+print(listaPrimos(a))
+# %%
+##3. Escreva uma função que receba duas listas e retorne outra lista com os elementos que estão presentes em apenas uma das listas.
+def listas_separadas(lst1, lst2):
+    lst_final = []
+    result = lst_final.append(lst1)
+    return result
+
+print(listas_separadas(a, b))
 
 #%%
 ## 4. Dada uma lista de números inteiros, escreva uma função para encontrar o segundo maior valor na lista
@@ -20,6 +47,32 @@ def seg_maior(n):
 
 resultado = seg_maior(lista)
 print(f'O segundo maior número dessa lista é {resultado}')
+
+# %%
+## 5. Crie uma função que receba uma lista de tuplas, cada uma contendo o nome e a idade de uma pessoa, e retorne a lista ordenada pelo nome das pessoas em ordem alfabética.
+
+def list_tuplas(lst):
+    resposta = sorted(lst, key=lambda lst:lst[0])
+    return resposta
+
+lista_de_tuplas = [('Anderson', 40), ('Renata', 45), ('Lucas', 27), ('Beatriz', 26)]
+list_tuplas(lista_de_tuplas)
+
+# %%
+## 6. Observe os espaços sublinhados e complete o código
+
+import __________.pyplot as plt
+import numpy as ___
+fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(5.5, 3.5),
+ layout="constrained")
+for ___ in range(2):
+ for ___ in range(2):
+ axs[row, col].annotate(f'axs[{row}, {col}]', (0.5, 0.5),
+ transform=axs[row, col].transAxes,
+ ha='center', va='center', ________=18,
+ color='darkgrey')
+fig.suptitle('__.subplots()')
+
 
 #%%
 ## 8. Utilizando pandas, como realizar a leitura de um arquivoA CSV em um DataFrame e exibir as primeiras linhas?
