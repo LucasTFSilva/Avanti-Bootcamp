9a8efe1259ce5c5f8d0b3bd08dde90bae9c82aa2
Lucas Teixeira <lucasteixeira8784@gmail.com>
2025-03-25 18:59:58 -0300

    Finalização exercícios

--- Atividade_02.py
+++ Atividade_02.py
@@ -61,18 +61,16 @@ list_tuplas(lista_de_tuplas)
 # %%
 ## 6. Observe os espaços sublinhados e complete o código
 
-import __________.pyplot as plt
-import numpy as ___
+import matplotlib.pyplot as plt
+import numpy as np
 fig, axs = plt.subplots(ncols=2, nrows=2, figsize=(5.5, 3.5),
  layout="constrained")
-for ___ in range(2):
- for ___ in range(2):
- axs[row, col].annotate(f'axs[{row}, {col}]', (0.5, 0.5),
- transform=axs[row, col].transAxes,
- ha='center', va='center', ________=18,
- color='darkgrey')
-fig.suptitle('__.subplots()')
-
+for row in range(2):
+        for col in range(2):
+            axs[row, col].annotate(f'axs[{row}, {col}]', (0.5, 0.5),
+            transform=axs[row, col].transAxes, ha='center', va='center', 
+            fontsize=18, color='darkgrey')
+fig.suptitle('plt.subplots()')
 
 #%%
 ## 8. Utilizando pandas, como realizar a leitura de um arquivoA CSV em um DataFrame e exibir as primeiras linhas?
